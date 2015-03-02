ff583196f8e567dd4c1a2e3bace66a2164caa6cc
Deplax <Deplax@10.73.38.204>
2015-03-02 21:24:08 +0900

    clear05

---
+++ src/main/java/next/controller/SaveController.java
@@ -0,0 +1,27 @@
+package next.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import next.dao.QuestionDao;
+import next.model.Question;
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+
+public class SaveController extends AbstractController{
+
+	private QuestionDao questionDao = new QuestionDao();
+	
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		String writer = request.getParameter("writer");
+		String title = request.getParameter("title");
+		String contents = request.getParameter("contents");
+
+		questionDao.insert(new Question(writer, title, contents));
+		
+		ModelAndView mav = jstlView("redirect:/list.next");
+		return mav;
+	}
+}
